e45c8939ed4829c54cdd83aff68ad9a8e76093ec
gabyoliveeira <santoligaby06@gmail.com>
2025-03-03 14:32:27 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,62 @@
+let listadeNumerosSorteados=[];
+let numeroLimite=10;
+let numeroSecreto=gerarNumerAleatorio();
+let tentativas=1;
+
+function exibirTextonaTela(tag, texto){
+    let campo=document.querySelector(tag);
+    campo.innerHTML=texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+function exibirMenagemInicial(){
+    exibirTextonaTela('h1', 'Jogo do número secreto');
+    exibirTextonaTela('p', 'Escolha um número entre 1 e 10');
+}
+exibirMenagemInicial();
+
+function verificarChute(){
+    let chute=document.querySelector('input').value;
+    if (chute==numeroSecreto){
+        let palavraTentativa=tentativas>1?'tentativas':'tentativa';
+        let mensagemTentativas=`Você acertou o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextonaTela('h1', 'Parabéns!');
+        exibirTextonaTela('p',mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        if (chute>numeroSecreto){
+            exibirTextonaTela('p','Número secreto é menor.');
+        }else{
+            exibirTextonaTela('p', 'Número secreto é maior.');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumerAleatorio() {
+    let numeroEscolhido=parseInt(Math.random()*numeroLimite+1);
+    let quantidadeDeElementosNalista = listadeNumerosSorteados.length;
+    if (listadeNumerosSorteados.length==numeroLimite){
+        listadeNumerosSorteados=[];
+    }
+    if (listadeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumerAleatorio();
+    } else{
+        listadeNumerosSorteados.push(numeroEscolhido);
+        console.log(listadeNumerosSorteados);
+        return numeroEscolhido;
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value='';
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumerAleatorio(); // Chama a função para gerar um novo número
+    limparCampo();
+    tentativas = 1;
+    exibirMenagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true); // true pq preciso que ele esteja desabilitado
+}
---
+++ js-curso-2-desafio_1/app.js
@@ -0,0 +1,13 @@
+let listaGenerica=[];
+
+let linguagensDeProgamacao=['Javascript', 'C','C++', 'Kotlin', 'Python'];
+linguagensDeProgamacao.push('Java', 'Ruby', 'GoLang');
+
+let tresnomes=['Javascript', 'Java', 'Python'];
+console.log(tresnomes[0]);
+
+let tresnomes1=['Javascript', 'Java', 'Python'];
+console.log(tresnomes[1]);
+
+let tresnomes2=['Javascript', 'Java', 'Python'];
+console.log(tresnomes[2]);
